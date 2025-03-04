61c4d4b2d1959300da52b4aa9ecd1f469dd59115
Shanafkhan <shanaf.khan@blenheimchalcot.com>
2025-03-04 17:30:19 +0530

    Added quesition component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,1 +1,24 @@
-export default function Question() {}
+import QuestionTimer from "./QuestionTimer.jsx";
+import Answers from "./Answers.jsx";
+export default function Question() {
+  return (
+    <div id="question">
+      <QuestionTimer
+        key={activeQuestionIndex}
+        timeout={10000}
+        onTimeout={handleSkipAnswer}
+      />
+      {/**Here we add the key so that the component is reloaded
+       * evrery time the key is changed, if the key is not passed question timer will execute once remain as it is for the next questions
+       */}
+      <h2> {QUESTIONS[activeQuestionIndex].text}</h2>
+      <Answers
+        key={activeQuestionIndex}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        selectedAnswer={userAnswer[userAnswer.length - 1]}
+        answerState={answerState}
+        onSelect={handleSelectedAnswer}
+      />
+    </div>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback, useRef } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
 export default function Quiz() {
   const [answerState, setAnswerState] = useState("");
   const [userAnswer, setUserAnswer] = useState([]);
@@ -46,24 +45,7 @@ export default function Quiz() {
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex}
-          timeout={10000}
-          onTimeout={handleSkipAnswer}
-        />
-        {/**Here we add the key so that the component is reloaded
-         * evrery time the key is changed, if the key is not passed question timer will execute once remain as it is for the next questions
-         */}
-        <h2> {QUESTIONS[activeQuestionIndex].text}</h2>
-        <Answers
-          key={activeQuestionIndex}
-          answers={QUESTIONS[activeQuestionIndex].answers}
-          selectedAnswer={userAnswer[userAnswer.length - 1]}
-          answerState={answerState}
-          onSelect={handleSelectedAnswer}
-        />
-      </div>
+      <Question />
     </div>
   );
 }
